bc81023b600a31a99c1681e266b2c27521b90fa0
Pat <pat@Pats-MacBook-Pro.local>
2019-04-25 14:38:37 +0700

    add randomly render frog function

--- scratch4.py
+++ scratch4.py
@@ -1,14 +1,19 @@
 import arcade
 import os
 import random
+import math
 
-SPRITE_SCALING = 0.5
+SPRITE_SCALING = 0.1
+SPRITE_SCALING_COIN = 0.3
+COIN_COUNT = 40
+COIN_SPEED = 0.5
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Better Move Sprite with Keyboard Example"
+SCREEN_TITLE = "GU MAI AOW LEAW AI SU"
 
 MOVEMENT_SPEED = 5
+SPRITE_SPEED = 0.5
 def make_star_field(star_count):
     """ Make a bunch of circles for stars. """
 
@@ -97,6 +102,48 @@ def make_skyline(width, skyline_height, skyline_color,
 
     return shape_list
 
+class Coin(arcade.Sprite):
+    """
+    This class represents the coins on our screen. It is a child class of
+    the arcade library's "Sprite" class.
+    """
+
+    def follow_sprite(self, player_sprite):
+        """
+        This function will move the current sprite towards whatever
+        other sprite is specified as a parameter.
+
+        We use the 'min' function here to get the sprite to line up with
+        the target sprite, and not jump around if the sprite is not off
+        an exact multiple of SPRITE_SPEED.
+        """
+
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Random 1 in 100 chance that we'll change from our old direction and
+        # then re-aim toward the player
+        if random.randrange(100) == 0:
+            start_x = self.center_x
+            start_y = self.center_y
+
+            # Get the destination location for the bullet
+            dest_x = player_sprite.center_x
+            dest_y = player_sprite.center_y
+
+            # Do math to calculate how to get the bullet to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the bullet will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            # Taking into account the angle, calculate our change_x
+            # and change_y. Velocity is how fast the bullet travels.
+            self.change_x = math.cos(angle) * COIN_SPEED
+            self.change_y = math.sin(angle) * COIN_SPEED
+
+
 
 class Player(arcade.Sprite):
 
@@ -140,7 +187,7 @@ class MyGame(arcade.Window):
 
         # Variables that will hold sprite lists
         self.player_list = None
-
+        self.coin_list = None
         # Set up the player info
         self.player_sprite = None
 
@@ -149,9 +196,13 @@ class MyGame(arcade.Window):
         self.right_pressed = False
         self.up_pressed = False
         self.down_pressed = False
+        self.score = 0
+
+        # Don't show the mouse cursor
+        self.set_mouse_visible(False)
 
         # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(arcade.color.BLACK)
 
 
     def setup(self):
@@ -159,13 +210,42 @@ class MyGame(arcade.Window):
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        # Score
+        self.score = 0
+
 
         # Set up the player
         self.player_sprite = Player("images/character.png", SPRITE_SCALING)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
+        # Create the coins
+        for i in range(COIN_COUNT):
 
+            # Create the coin instance
+            frog_face = random.randint(1,8)
+            if frog_face == 1:
+                coin = Coin("images/frog/frog1.png", SPRITE_SCALING_COIN)
+            if frog_face == 2:
+                coin = Coin("images/frog/frog2.png", SPRITE_SCALING_COIN)
+            if frog_face == 3:
+                coin = Coin("images/frog/frog3.png", SPRITE_SCALING_COIN)
+            if frog_face == 4:
+                coin = Coin("images/frog/frog4.png", SPRITE_SCALING_COIN)
+            if frog_face == 5:
+                coin = Coin("images/frog/frog5.png", SPRITE_SCALING_COIN)
+            if frog_face == 6:
+                coin = Coin("images/frog/frog6.png", SPRITE_SCALING_COIN)
+            if frog_face == 7:
+                coin = Coin("images/frog/frog7.png", SPRITE_SCALING_COIN)
+            # Position the coin
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
     def on_draw(self):
         """
         Render the screen.
@@ -179,10 +259,27 @@ class MyGame(arcade.Window):
         self.skyline1.draw()
         self.skyline2.draw()
         self.player_list.draw()
+
+        self.coin_list.draw()
+        self.player_list.draw()
+
+        # Put the text on the screen.
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
         
 
     def update(self, delta_time):
         """ Movement and game logic """
+        for coin in self.coin_list:
+            coin.follow_sprite(self.player_sprite)
+
+        # Generate a list of all sprites that collided with the player.
+        hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        # Loop through each colliding sprite, remove it, and add to the score.
+        for coin in hit_list:
+            coin.kill()
+            self.score += 1
 
         # Calculate speed based on the keys pressed
         self.skyline1.center_x -= 0.5
